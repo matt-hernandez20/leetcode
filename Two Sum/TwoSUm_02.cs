3618aa8168bdd3cf31180a7a506a5ebe3f668ecc
mhernandez42 <matt.hernandez2020@gmail.com>
2020-02-24 01:16:18 -0600

    Two Sum submissions.

---
+++ Two Sum/TwoSUm_02.cs
@@ -0,0 +1,36 @@
+﻿//Runtime: 240 ms, faster than 89.94% of C# online submissions for Two Sum.
+//Memory Usage: 31.2 MB, less than 5.08% of C# online submissions for Two Sum.
+public class Solution
+{
+
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+
+        if (nums == null || nums.Length < 2)
+            return result;
+
+        Dictionary<int, int> hash = new Dictionary<int, int>();
+
+        hash.Add(nums[0], 0);
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+
+            if (hash.TryGetValue(target - nums[i], out result[0]))
+            {
+                result[1] = i;
+                return result;
+            }
+            else if (!hash.TryGetValue(nums[i], out int temp))
+            {
+                hash.Add(nums[i], i);
+            }
+
+
+
+        }
+        return result;
+    }
+
+}
---
+++ Two Sum/TwoSum_01.cs
@@ -0,0 +1,27 @@
+﻿//Runtime: 492 ms, faster than 17.78% of C# online submissions for Two Sum.
+//Memory Usage: 30.7 MB
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (EqualToTarget(nums[i], nums[j], target))
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    public bool EqualToTarget(int numOne, int numTwo, int target)
+    {
+        return numOne + numTwo == target;
+    }
+}
