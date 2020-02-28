7980c10e4377e2b0298ba13249582bf869b2486b
mhernandez42 <matt.hernandez2020@gmail.com>
2020-02-28 02:18:18 -0600

    First submission for Add Two Numbers challenge.

--- Add Two Numbers/AddTwoNum_01.cs
+++ Add Two Numbers/AddTwoNum_01.cs
@@ -1,63 +1,150 @@
-﻿using System;
+﻿//Runtime: 112 ms, faster than 51.48% of C# online submissions for Add Two Numbers.
+//Memory Usage: 27.8 MB, less than 9.09% of C# online submissions for Add Two Numbers.
 
-  //Definition for singly-linked list.
-public class ListNode
-{
-      public int val;
-      public ListNode next;
-      public ListNode(int x) { val = x; }
-}
+//NOTES: Need to review recursion
+//       Review datatypes that are either passed by reference or value
+//       https://jonskeet.uk/csharp/parameters.html
 
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
 public class Solution
 {
 
-    private ListNode _resultList;
-
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        if (l1 == null || l2 == null)
-            return null;
+        //dictionary to hold value of each node value in list #1, key = index
+        Dictionary<int, int> l1Dict = new Dictionary<int, int>();
+        l1Dict.Add(0, l1.val);
+        if (l1.next != null)
+            BuildValueList(l1.next, 0,  l1Dict);
 
-        int carryOne = 0;
+        //dictionary to hold value of each node value in list #1, key = index
+        Dictionary<int, int> l2Dict = new Dictionary<int, int>();
+        l2Dict.Add(0, l2.val);
+        if (l1.next != null)
+            BuildValueList(l2.next, 0,  l2Dict);
 
-        ListNode resultNode;
 
-        int sum = l1.val + l2.val;
-        if (sum <= 9)
+        int l1DictSize = l1Dict.Count;
+        int l2DictSize = l2Dict.Count;
+
+        int sizeDiff = l1DictSize - l2DictSize;
+
+        Dictionary<int, int> resultDict = new Dictionary<int, int>();
+
+        int remainder = 0;
+
+        //for when the input ListNodes are unequal and equal in length
+        if (sizeDiff < 0)
         {
-            resultNode = new ListNode(sum);
+            //l2 size bigger
+            remainder = GetSum(l1Dict, l2Dict, l1DictSize,  resultDict);
+            GetSingleListSum(l2Dict, l1DictSize, l2DictSize, remainder,  resultDict);
+        }
+        else if (sizeDiff > 0)
+        {
+            remainder = GetSum(l1Dict, l2Dict, l2DictSize,  resultDict);
+            GetSingleListSum(l1Dict, l2DictSize, l1DictSize, remainder,  resultDict);
         }
         else
         {
-            resultNode = new ListNode(sum - 10);
-            carryOne = 1;
+            remainder = GetSum(l1Dict, l2Dict, l2DictSize,  resultDict);
+            resultDict.Add(l2DictSize, remainder);
         }
 
-        if (_resultList == null)
-            _resultList = resultNode;
+        //ListNode resultListNode = new ListNode(resultDict[0]);
 
+        ListNode resultListNode = CreateListNodeLinkedList(null, 0, resultDict.Count,  resultDict);
 
+        return resultListNode;
+    }
 
-        if (l1.next != null && l2.next != null)
+    //Use recursion to create new ListNode for output, 
+    //index is for traverse until max (count of dictionary of result values)
+    public static ListNode CreateListNodeLinkedList(ListNode listNode, int index, int max,  Dictionary<int, int> resultDict)
+    {
+        if (index == max - 1)
+            return listNode;
+
+        listNode = new ListNode(resultDict[index]);
+
+        listNode.next = CreateListNodeLinkedList(listNode.next, index += 1, max,  resultDict);
+
+        return listNode;
+    }
+
+    //for use case when one node list is greater in length than the other
+    public void GetSingleListSum(Dictionary<int, int> listDictionary, int startIndex, int endIndex, int remainder,  Dictionary<int, int> resultDict)
+    {
+        //start index is after 'shorter' node list ended, 
+        //iterator ends at count of 'longer' node list
+        for (int i = startIndex; i < endIndex; i++)
         {
-            l1.next.val += carryOne;
-            _resultList.next = AddTwoNumbers(l1.next, l2.next);
-            return _resultList;
-        }
 
-        if(l1.next == null)
-        {
-            ListNode listNodeItem = l2.next;
-            listNodeItem.val += carryOne;
-            while(listNodeItem != null)
+            int sum = listDictionary[i] += remainder;
+
+            if (listDictionary[i] > 9)
             {
+                resultDict.Add(i, sum - 10);
+                remainder = 1;
+            }
+            else
+            {
+                resultDict.Add(i, sum);
+                remainder = 0;
+            }
 
-                _resultList.next
+        }
+        if (remainder > 0)
+            resultDict.Add(endIndex, remainder);
+
+    }
+
+    //sum of two node values at same key index among the two dictionaries. 
+    public int GetSum(Dictionary<int, int> l1Dict, Dictionary<int, int> l2Dict, int maxSize,  Dictionary<int, int> resultDict)
+    {
+        //flag for if sum results in need to 'carry the one' to next node value
+        bool carryTheOne = false;
+
+        for (int i = 0; i < maxSize; i++)
+        {
+
+            int sum = l1Dict[i] + l2Dict[i];
+
+            if (carryTheOne)
+                sum += 1;
+
+            if (sum > 9)
+            {
+                resultDict.Add(i, sum - 10);
+                carryTheOne = true;
+            }
+            else
+            {
+                resultDict.Add(i, sum);
+                carryTheOne = false;
             }
         }
+        if (carryTheOne)
+            return 1;
+        return 0;
+    }
 
-
-
+    //Use recursion to traverse linked list, incrementing an index to add as key to dict  
+    public void BuildValueList(ListNode node, int index, Dictionary<int, int> dict)
+    {
+        if (node == null)
+            return;
+        index += 1;
+        dict.Add(index, node.val);
+        if (node.next == null)
+            return;
+        BuildValueList(node.next, index,  dict);
     }
 }
